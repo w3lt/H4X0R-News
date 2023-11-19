ccd75b1472d34f5e1808ed390c9cbfea1b3adcad
John Silver <tienduy0123@gmail.com>
2023-11-19 17:27:13 +0100

    Initial Commit

---
+++ H4X0R/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  H4X0R
+//
+//  Created by John Silver on 19/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ H4X0R/H4X0RApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4X0RApp.swift
+//  H4X0R
+//
+//  Created by John Silver on 19/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct H4X0RApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
